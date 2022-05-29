d6907494b91f0f174cae5ddf9a2f95065b6407a5
Ryan Salomão <ryansalomao@gmail.com>
2022-05-28 23:41:12 -0300

    add computer repository display method

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -47,6 +47,7 @@ class ComputerRepository
 
         command.ExecuteNonQuery();
         connection.Close();
+
         return computer;
     }
 
@@ -76,5 +77,23 @@ class ComputerRepository
 
         command.ExecuteNonQuery();
         connection.Close();
+    }
+
+    public Computer Display(int id)
+    {        
+        var connection = new SqliteConnection(databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers WHERE id = $id;";
+        command.Parameters.AddWithValue("$id", id);
+
+        var reader = command.ExecuteReader();
+        reader.Read();
+
+        var computer = new Computer(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));           
+        connection.Close();
+
+        return computer;
     }
 }
